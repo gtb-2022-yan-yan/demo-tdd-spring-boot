1d179788dbe49297f78a0ec7b8b2d9448d2dfe1b
Yan Yan <yyan1212chn@gmail.com>
2022-02-19 23:11:55 +0800

    feat: generate project skeleton

---
+++ src/main/java/com/tw/capability/gtb/demotddspringboot/DemoTddSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.tw.capability.gtb.demotddspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoTddSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoTddSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tw/capability/gtb/demotddspringboot/DemoTddSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tw.capability.gtb.demotddspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoTddSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
