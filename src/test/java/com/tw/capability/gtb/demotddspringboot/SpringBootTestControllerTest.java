2c7e0fac7ac0cee229638af27adb4111a5c7e865
Yan Yan <yyan1212chn@gmail.com>
2022-02-19 23:49:38 +0800

    refactor: improve sonarlint warnings

--- src/test/java/com/tw/capability/gtb/demotddspringboot/SpringBootTestControllerTest.java
+++ src/test/java/com/tw/capability/gtb/demotddspringboot/SpringBootTestControllerTest.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class SpringBootTestControllerTest {
+class SpringBootTestControllerTest {
 
     @Autowired
     private TestRestTemplate restTemplate;
