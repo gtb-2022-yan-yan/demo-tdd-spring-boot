353e49ff611aaf91858d66215119450c2f5575f1
Yan Yan <yyan1212chn@gmail.com>
2022-02-21 00:37:06 +0800

    feat: return internal server error when throw exception

---
+++ src/main/java/com/tw/capability/gtb/demotddspringboot/ErrorResult.java
@@ -0,0 +1,13 @@
+package com.tw.capability.gtb.demotddspringboot;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorResult {
+    private String errorMessage;
+
+}
---
+++ src/main/java/com/tw/capability/gtb/demotddspringboot/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.tw.capability.gtb.demotddspringboot;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ResponseBody
+    public ErrorResult handle(Exception e) {
+        return new ErrorResult(e.getMessage());
+
+    }
+}
